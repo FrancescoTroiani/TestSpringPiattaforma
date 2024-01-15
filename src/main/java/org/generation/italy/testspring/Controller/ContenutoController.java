7847c291d9497fee6b4a98b292abfe1bc6cc11fd
ROSHER185 <rosher185@gmail.com>
2024-01-15 10:50:21 +0100

    Primo commit

---
+++ src/main/java/org/generation/italy/testspring/Controller/ContenutoController.java
@@ -0,0 +1,68 @@
+package org.generation.italy.testspring.Controller;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import org.generation.italy.testspring.model.Contenuto;
+import org.generation.italy.testspring.repository.ContenutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/Contenuto")
+
+public class ContenutoController{
+	
+	@Autowired			
+	ContenutoRepository contenutoRepository;
+	
+	@GetMapping
+	@ResponseBody
+	public String index() {
+		return ("Benvenuto nella gestione della piattaforma");
+	}
+	
+	@GetMapping("/elenco")
+	@ResponseBody
+	public String elencoContenuti(
+			@RequestParam(required=false) String titolo,
+			@RequestParam(required=false) String tipologia,
+			@RequestParam(required=false) String genere,
+			@RequestParam(required=false) Integer annoDiProduzionePartenza,
+			@RequestParam(required=false) Integer annoDiProduzioneArrivo,
+			@RequestParam(required=false) String ordinamento) throws Exception {
+		
+		
+		ArrayList<Contenuto> elencoContenuto = new ArrayList<>();
+		if (titolo == null && tipologia == null && genere == null && annoDiProduzionePartenza == null)
+			elencoContenuto = (ArrayList<Contenuto>) contenutoRepository.findAll();
+		else if (titolo != null && tipologia == null && genere == null)
+			elencoContenuto = (ArrayList<Contenuto>) contenutoRepository.findByTitoloLike("%" + titolo + "%");
+		 else if (genere != null && tipologia == null && titolo == null)
+			elencoContenuto = (ArrayList<Contenuto>) contenutoRepository.findByGenere(genere);
+		else if (tipologia != null && genere == null && titolo == null)
+			elencoContenuto = (ArrayList<Contenuto>) contenutoRepository.findByTipologia(tipologia);
+		else if (annoDiProduzionePartenza != null && genere == null && titolo == null)
+			elencoContenuto = (ArrayList<Contenuto>) contenutoRepository
+					.findByAnnoDiProduzioneBetween(annoDiProduzionePartenza, annoDiProduzioneArrivo);
+		
+		if (ordinamento != null) {
+			if (ordinamento.equals("asc"))
+				Collections.sort(elencoContenuto);
+			else if (ordinamento.equals("desc"))
+				Collections.sort(elencoContenuto, Collections.reverseOrder());
+			else
+				throw new Exception("Ordinamento non valido");
+		}
+
+		StringBuilder elenco = new StringBuilder();
+		for (Contenuto c : elencoContenuto)
+			elenco.append(c.toString() + "<br>");
+		return elenco.toString();
+
+}
+}
